489d76aee501149b5bcaf05b643c0e8b15f06d48
Yasas <ysenarath.93@gmail.com>
2021-02-14 12:35:53 -0500

    Update version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('packages.txt') as fh:
 
 setuptools.setup(
     name="synet",
-    version="0.0.1",
+    version="0.0.2",
     author="Yasas Senarath",
     author_email="ysenarath.93@gmail.com",
     description="Knowledge API for Natural Language Processing",
