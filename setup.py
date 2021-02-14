e220718a4b1c0958066846f2b357d5226ce16ba7
Yasas <ysenarath.93@gmail.com>
2021-02-14 12:27:37 -0500

    Rename package name to synet

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('packages.txt') as fh:
     requirements = fh.read().strip().split('\n')
 
 setuptools.setup(
-    name="cynet",
+    name="synet",
     version="0.0.1",
     author="Yasas Senarath",
     author_email="ysenarath.93@gmail.com",
